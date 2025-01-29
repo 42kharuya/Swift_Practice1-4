96f93a84b2f513d7296f00ef396cf27ff7e8ca9c
42kharuya <haruya.0411.k@gmail.com>
2025-01-29 14:55:37 +0900

    Initial Commit

---
+++ Swift_Practice1-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Practice1-4
+//
+//  Created by 川﨑遥也 on 2025/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift_Practice1-4Tests/Swift_Practice1_4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice1_4Tests.swift
+//  Swift_Practice1-4Tests
+//
+//  Created by 川﨑遥也 on 2025/01/29.
+//
+
+import Testing
+@testable import Swift_Practice1_4
+
+struct Swift_Practice1_4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
